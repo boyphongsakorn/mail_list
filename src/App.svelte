7c63d7488b5a2e91f0da2c8610661ea381eab5c7
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-10 20:58:24 +0700

    test

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+  //import svelteLogo from './assets/svelte.svg'
+  //import Counter from './lib/Counter.svelte'
+  //get json from mkrm.pwisetthon.com
+  var list = [];
+  (async () => {
+    const response = await fetch('https://anywhere.pwisetthon.com/https://mkrm.pwisetthon.com/list_show.php');
+    const data = await response.json();
+    console.log(data);
+    list = data;
+  })();
+</script>
+
+<main>
+  <table>
+    <tr>
+      <th>ชื่อ</th>
+      <th>ชื่อเล่น</th>
+      <th>ที่อยู่</th>
+    </tr>
+    {#each list as player}
+      <tr>
+        <td>{player.name}</td>
+        <td>{player.nickname}</td>
+        <td>{player.address}</td>
+      </tr>
+    {/each}
+  </table>
+  เพิ่มรายชื่อ
+  <form action="https://mkrm.pwisetthon.com/list_add.php" method="post">
+    <input type="text" name="name" placeholder="Name">
+    <input type="text" name="nickname" placeholder="Nickname">
+    <input type="text" name="address" placeholder="Address">
+    <input type="submit" value="Add">
+  </form>
+</main>
+
+<style>
+  table {
+    border-collapse: collapse;
+    border: 1px solid #ccc;
+  }
+  tr {
+    border-collapse: collapse;
+    border: 1px solid #ccc;
+  }
+  th {
+    border-collapse: collapse;
+    border: 1px solid #ccc;
+    padding: 5px;
+  }
+  td {
+    border-collapse: collapse;
+    border: 1px solid #ccc;
+    padding: 5px;
+  }
+</style>
